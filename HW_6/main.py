60641b27307e7c16691ea894fcdeabe8fe7178d4
Marat <email.marat@gmail.com>
2023-09-30 14:25:00 +0300

    added HW_6

--- HW_6/main.py
+++
@@ -1,9 +0,0 @@
-from diff_lists import DiffLists
-
-
-lst1 = DiffLists()
-lst2 = DiffLists()
-lst1.setListOne([1, 3, 7, 8])
-lst2.setListTwo([2, 5, 7, 2, 7])
-result = DiffLists()
-result.diff_lists()
